f3ba4bdce9caa65ff1d8fb4f5114941576e89588
Marleess Windows <marleysumarly@gmail.com>
2022-10-15 03:27:46 +0700

    Controller

---
+++ app/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Belajar\Mvc\Controller;
+
+class HomeController
+{
+    public function index(): void
+    {
+    echo "Index";
+    }
+
+    public function hello(): void
+    {
+    echo "Hello";
+    }
+
+    public function world(): void
+    {
+    echo "World";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
 
-$path = "/";
+require_once __DIR__ . '/../vendor/autoload.php';
+use Belajar\Mvc\App\Router;
+use Belajar\Mvc\Controller\HomeController;
 
-if (isset($_SERVER['PATH_INFO'])) {
-    $path = $_SERVER['PATH_INFO'];
-}
-require __DIR__ . "/../app/views" . $path . ".php";
+Router::get("GET", "/", HomeController::class, "index");
+Router::get("GET", "/hello", HomeController::class, "hello");
+Router::get("GET", "/world", HomeController::class, "world");
+Router::run();
